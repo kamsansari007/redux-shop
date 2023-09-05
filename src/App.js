4c35c919b66235df95d85dd2b818fd5f7380c0dd
kamsansari007 <kamsansari007@gmail.com>
2023-09-05 15:24:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route path="*">404 Not Found</Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
